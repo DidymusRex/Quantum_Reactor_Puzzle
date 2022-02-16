391a5b7ea8a74e028c5ba2c8e945e80d00bafc39
John Grover <d.rex@didymusrex.net>
2022-02-15 19:58:55 -0500

    Updated beacon comments

--- boot_ble_beacon.py
+++ boot_ble_beacon.py
@@ -1,6 +1,14 @@
 """
 Bluetooth beacon
 save as boot.py on beacon device
+    requires basic_ble.py on the device as well
+
+Waits for someone to connect and send message 'find me'
+When the message is recieved begin flashing bright LED for 5 min
+
+Quadcorder should get within BLE distance and scan for beacons
+   QC will connect and send 'find me' message so players can see
+   where the beacon is located.
 """
 from basic_ble import *
 from machine import Pin, PWM, reset, Timer
@@ -10,7 +18,8 @@ from time import sleep_ms
 def reboot():
     reset()
 
-# set up BLE object
+# set up BLE object. Needs to be unique per beacon.
+#  To do: make hardware definable (i.e specific pins set to HI/LO)
 ble = BLE('key_can_one')
 
 # set up leds
@@ -20,20 +29,23 @@ flasher=Pin(13,Pin.OUT)
 # set up boot pin
 btn=Pin(0,Pin.IN)
 
-# ISR for boot pin
+# ISR for boot pin - Pressing ESP32 dev board boot pin resets flasher
+#   mostly for testing/debugging
 def btn_isr(pin):
     if flashing:
         timerF.deinit()
         timerX.deinit()
         flasher(0)
 
-# attache ISR to boot pin
+# attach ISR to boot pin
 btn.irq(trigger=Pin.IRQ_FALLING, handler=btn_isr)
 
 # initialize flashing state
 flashing=False
 
 # Timer(0) used by basic_ble.py
+# Timer(3) used by KeyPad.py
+#    minimal risk of collision, but play it safe we only have 0-3
 timerF=Timer(1)
 timerX=Timer(2)
 
@@ -64,10 +76,11 @@ while True:
         print('begin flashing')
         # flash the beacon
         timerF.init(period=200, mode=Timer.PERIODIC, callback=lambda t: flasher(not flasher.value()))
-        # for 5 minutes: 300000. 5000 (5 sec. for testing)
+        # ... for 5 minutes: 300000. 5000 (5 sec. for testing)
         timerX.init(period=5000, mode=Timer.ONE_SHOT, callback=stop_flashing)
         ble.send('lit up!')
-        
+    
+    # < debugging and monitoring stuff
     elif ble.event_msg == 'foo':
         print('init event message')
         ble.event_msg = 'bar'
@@ -79,5 +92,6 @@ while True:
         print('invalid message ' + ble.event_msg)
         ble.event_msg = 'bar'
         ble.send('try again, hombre')
+    # > debugging and monitoring stuff
 
     sleep_ms(100)
